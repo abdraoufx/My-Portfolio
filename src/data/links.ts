ecebd14944ca06a22b8cd58beb5b9f59ed2c701b
abdraoufx <abdr28609@gmail.com>
2023-06-07 14:37:38 +0100

    contains app urls

---
+++ src/data/links.ts
@@ -0,0 +1,7 @@
+export const urls = {
+  upworkURL: "https://www.upwork.com/freelancers/~0126c518b8543236eb",
+  fbURL: "https://www.facebook.com/AbdRaouf.zk/",
+  twitterURL: "https://twitter.com/abdraoufx",
+  githubURL: "https://github.com/abdraoufx",
+  frontEndMentorURL: "https://www.frontendmentor.io/profile/abdraoufx",
+};
